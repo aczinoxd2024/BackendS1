cd98a10752a5d633850caef70321a823bcae0f16
aczinoxd2024 <Luis>
2025-05-05 18:20:07 -0400

    actualizacion adquirir menbresia

--- src/auth/dto/clienteCrear.dto.ts
+++ src/auth/dto/clienteCrear.dto.ts
@@ -1,12 +1,34 @@
+import { IsString, IsOptional, IsDateString, IsNumber } from 'class-validator';
+
 export class ClienteCrearDto {
+  @IsString()
   ci: string;
+
+  @IsString()
   nombre: string;
+
+  @IsString()
   apellido: string;
+
+  @IsDateString()
   fechaNacimiento: Date;
+
+  @IsString()
   telefono: string;
+
+  @IsString()
   direccion: string;
+
+  @IsOptional()
+  @IsString()
   observacion?: string;
+
+  @IsString()
   correo: string;
+
+  @IsNumber()
   tipoMembresiaId: number;
+
+  @IsNumber()
   metodoPagoId: number;
 }
