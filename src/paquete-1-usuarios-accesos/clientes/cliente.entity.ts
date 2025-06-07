d3ce04092fe4bfaf72837edd9a3b25bda0a38823
aczinoxd2024 <Blancobautistaluisfernando@gmail.com>
2025-06-07 16:39:43 -0400

    solucion

--- src/paquete-1-usuarios-accesos/clientes/cliente.entity.ts
+++ src/paquete-1-usuarios-accesos/clientes/cliente.entity.ts
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import { SeguimientoCliente } from 'paquete-3-control-comercial/seguimiento/seguimiento.entity';
-import { Entity, Column, PrimaryColumn, OneToMany } from 'typeorm';
-=======
 import { Entity, Column, PrimaryColumn, OneToMany } from 'typeorm';
 import { Usuario } from 'paquete-1-usuarios-accesos/usuarios/usuario.entity';
 import { ManyToOne, JoinColumn } from 'typeorm';
 import { SeguimientoCliente } from 'paquete-3-control-comercial/seguimiento-cliente/seguimiento-cliente.entity';
-
-
->>>>>>> 64493db44f2098134be71f5d149468551edc4139
 
 @Entity()
 export class Cliente {
@@ -21,13 +14,6 @@ export class Cliente {
   @Column('text')
   Observacion: string;
 
-<<<<<<< HEAD
-  @OneToMany(() => SeguimientoCliente, (s) => s.cliente)
-  seguimientos: SeguimientoCliente[];
-=======
   @OneToMany(() => SeguimientoCliente, (s) => s.IDCliente)
-seguimientos: SeguimientoCliente[];
-
-
->>>>>>> 64493db44f2098134be71f5d149468551edc4139
+  seguimientos: SeguimientoCliente[];
 }
