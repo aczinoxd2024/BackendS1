cbffd346f53a3b380f732ba9c33e08b79e672f76
hebertsb <suarezburgoshebert@gmail.com>
2025-06-16 15:47:43 -0400

    Habilitar /api/dias para frontend

--- src/paquete-2-servicios-gimnasio/dia-semana/dia-semana.module.ts
+++ src/paquete-2-servicios-gimnasio/dia-semana/dia-semana.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DiaSemana } from './dia-semana.entity';
 import { DiaSemanaService } from './dia-semana.service';
 import { DiaSemanaController } from './dia-semana.controller';
+import { DiasController } from './dias.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([DiaSemana])],
-  controllers: [DiaSemanaController],
+  controllers: [DiaSemanaController, DiasController],
   providers: [DiaSemanaService],
   exports: [TypeOrmModule],
 })
