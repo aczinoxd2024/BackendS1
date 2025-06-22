17bedbfa283c4ff0a6c5d7084dc0b80ea1c8b898
aczinoxd2024 <Blancobautistaluisfernando@gmail.com>
2025-06-21 20:39:22 -0400

    nose que mas hacer

--- src/paquete-1-usuarios-accesos/clientes/clientes.controller.ts
+++ src/paquete-1-usuarios-accesos/clientes/clientes.controller.ts
@@ -109,8 +109,8 @@ export class ClientesController {
       );
     }
   }
-  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('cliente')
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get('perfil-renovar')
   async obtenerPerfilParaRenovacion(@Req() req: UserRequest) {
     const correo = req.user?.correo;
